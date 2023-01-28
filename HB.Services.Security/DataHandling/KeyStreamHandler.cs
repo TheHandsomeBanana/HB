a61f900e6cd0be3b29196141b241962bb6de91f7
Merwa Stefan <stefanbsns@gmail.com>
2023-01-28 20:23:57 +0100

    KeyFileHandler

---
+++ HB.Services.Security/DataHandling/KeyStreamHandler.cs
@@ -0,0 +1,118 @@
+﻿using HB.Common;
+using HB.Common.Json;
+using HB.Common.Streams;
+using HB.Services.Security.Cryptography.Keys;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Win32;
+using System.Security.Cryptography;
+
+namespace HB.Services.Security.DataHandling {
+    public class KeyStreamHandler : SecurityStreamHandler, ISecurityStreamHandler<IKey> {
+        private Type keyType;
+
+        public KeyStreamHandler(Type keyType) : base() {
+            this.keyType = keyType;
+        }
+
+        public KeyStreamHandler(FileStream stream, Type keyType) : base(stream) {
+            this.keyType = keyType;
+        }
+
+        public KeyStreamHandler(string filePath, Type keyType) : base(filePath) {
+            this.keyType = keyType;
+        }
+
+        public IKey? Read() {
+            //if(StreamMode == SecurityStreamMode.FileDialog) {
+            //    OpenFileDialog ofd = new OpenFileDialog();
+            //    ofd.Filter = "(*jk)|*jk";
+
+            //    if (ofd.ShowDialog() == true) {
+            //        Stream = new FileStream(ofd.FileName, FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            //    }
+            //}
+
+            string? json;
+            if (InstantDisposal) {
+                Stream = new FileStream(FilePath, FileMode.OpenOrCreate, FileAccess.ReadWrite);
+                json = GlobalEnvironment.GlobalEncoding.GetString(Stream.Read());
+                Stream.Dispose();
+            }
+            else {
+                json = GlobalEnvironment.GlobalEncoding.GetString(Stream.Read());
+                Stream.Position = 0;
+            }
+
+            return JsonConvert.DeserializeObject(json, keyType) as IKey;
+        }
+
+        public async Task<IKey?> ReadAsync() {
+            if (StreamMode == SecurityStreamMode.FileDialog)
+                throw new NotSupportedException($"{StreamMode} not supported in async execution.");
+
+            string? json;
+            if (InstantDisposal) {
+                Stream = new FileStream(FilePath, FileMode.OpenOrCreate, FileAccess.ReadWrite);
+                json = GlobalEnvironment.GlobalEncoding.GetString(await Stream.ReadAsync());
+                await Stream.DisposeAsync();
+            }
+            else {
+                json = GlobalEnvironment.GlobalEncoding.GetString(await Stream.ReadAsync());
+                Stream.Position = 0;
+            }
+
+            return JsonConvert.DeserializeObject(json, keyType) as IKey;
+        }
+
+        public void Write(IKey item) {
+            //if (StreamMode == SecurityStreamMode.FileDialog) {
+            //    SaveFileDialog sfd = new SaveFileDialog();
+            //    sfd.Filter = "(*jk)|*jk";
+
+            //    if (sfd.ShowDialog() == true) {
+            //        Stream = new FileStream(sfd.FileName, FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            //    }
+            //}
+
+            byte[] json = GlobalEnvironment.GlobalEncoding.GetBytes(JsonConvert.SerializeObject(item));
+
+            if (InstantDisposal) {
+                Stream = new FileStream(FilePath, FileMode.OpenOrCreate, FileAccess.ReadWrite);
+                Stream.Write(json);
+                Stream.Dispose();
+            }
+            else {
+                Stream.Write(json);
+                Stream.Position = 0;
+            }
+
+        }
+
+        public async Task WriteAsync(IKey item) {
+            if (StreamMode == SecurityStreamMode.FileDialog)
+                throw new NotSupportedException($"{StreamMode} not supported in async execution.");
+
+            byte[] json = GlobalEnvironment.GlobalEncoding.GetBytes(JsonConvert.SerializeObject(item));
+
+            if (InstantDisposal) {
+                Stream = new FileStream(FilePath, FileMode.OpenOrCreate, FileAccess.ReadWrite);
+                await Stream.WriteAsync(json);
+                await Stream.DisposeAsync();
+            }
+            else {
+                await Stream.WriteAsync(json);
+                Stream.Position = 0;
+            }
+        }
+
+        public void Dispose() {
+            this.Stream?.Dispose();
+        }
+    }
+}
--- HB.Services.Security/DataHandling/SecurityStreamHandler.cs
+++ HB.Services.Security/DataHandling/SecurityStreamHandler.cs
@@ -5,9 +5,51 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace HB.Services.Security.DataHandling {
-    public class SecurityStreamHandler {
-        public SecurityStreamHandler(Stream stream) {
+    public abstract class SecurityStreamHandler {
+        protected FileStream Stream { get; set; }
+        protected string FilePath { get; }
+        protected SecurityStreamMode StreamMode { get; }
 
+        private bool instantDisposal = true;
+        public bool InstantDisposal {
+            get {
+                return instantDisposal;
+            }
+            set {
+                if (value)
+                    Stream?.Dispose();
+
+                instantDisposal = value;
+            }
+        }
+
+        public SecurityStreamHandler() {
+            Stream = null;
+            StreamMode = SecurityStreamMode.FileDialog;
+        }
+
+        public SecurityStreamHandler(FileStream stream) {
+            instantDisposal = false;
+            FilePath = stream.Name;
+            Stream = stream;
+            StreamMode = SecurityStreamMode.InputStream;
+        }
+
+        public SecurityStreamHandler(string filePath) {
+            FilePath = filePath;
+
+            if (!InstantDisposal)
+                Stream = new FileStream(filePath, FileMode.OpenOrCreate, FileAccess.ReadWrite);
+
+            StreamMode = SecurityStreamMode.FilePath;
+        }
+
+        protected enum SecurityStreamMode {
+            InputStream,
+            FilePath,
+            FileDialog
         }
     }
+
+
 }
