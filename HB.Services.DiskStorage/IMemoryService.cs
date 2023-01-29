a411b2a9eeebcdb590f32232f01f4be265b35cfd
Merwa Stefan <stefanbsns@gmail.com>
2023-01-29 23:03:47 +0100

    Implement MemoryObject without encryption

---
+++ HB.Services.DiskStorage/IMemoryService.cs
@@ -0,0 +1,21 @@
+﻿using HB.Services.DiskStorage.Objects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HB.Services.DiskStorage
+{
+    public interface IMemoryService {
+        public MemoryObject ReadMemory(string location);
+        public Task<MemoryObject> ReadMemoryAsync(string location);
+        public void WriteMemory(MemoryObject memoryObject, string location);
+        public Task WriteMemoryAsync(MemoryObject memoryObject, string location);
+
+        public MemoryObject<TMemoryObject> ReadMemory<TMemoryObject>(string location);
+        public Task<MemoryObject<TMemoryObject>> ReadMemoryAsync<TMemoryObject>(string location);
+        public void WriteMemory<TMemoryObject>(MemoryObject<TMemoryObject> memoryObject, string location);
+        public Task WriteMemoryAsync<TMemoryObject>(MemoryObject<TMemoryObject> memoryObject, string location);
+    }
+}
--- HB.Services.DiskStorage/MemoryService.cs
+++ HB.Services.DiskStorage/MemoryService.cs
@@ -1,5 +1,37 @@
-﻿namespace HB.Services.DiskStorage {
-    public class MemoryService {
+﻿using HB.Services.DiskStorage.Objects;
 
+namespace HB.Services.DiskStorage {
+    public class MemoryService : IMemoryService {
+        public MemoryObject ReadMemory(string location) {
+            throw new NotImplementedException();
+        }
+
+        public MemoryObject<TMemoryObject> ReadMemory<TMemoryObject>(string location) {
+            throw new NotImplementedException();
+        }
+
+        public Task<MemoryObject> ReadMemoryAsync(string location) {
+            throw new NotImplementedException();
+        }
+
+        public Task<MemoryObject<TMemoryObject>> ReadMemoryAsync<TMemoryObject>(string location) {
+            throw new NotImplementedException();
+        }
+
+        public void WriteMemory(MemoryObject memoryObject, string location) {
+            throw new NotImplementedException();
+        }
+
+        public void WriteMemory<TMemoryObject>(MemoryObject<TMemoryObject> memoryObject, string location) {
+            throw new NotImplementedException();
+        }
+
+        public Task WriteMemoryAsync(MemoryObject memoryObject, string location) {
+            throw new NotImplementedException();
+        }
+
+        public Task WriteMemoryAsync<TMemoryObject>(MemoryObject<TMemoryObject> memoryObject, string location) {
+            throw new NotImplementedException();
+        }
     }
 }
