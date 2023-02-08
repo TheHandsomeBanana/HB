2d7e8f46c47794d9ca5e6191f753eb91b9c5aad7
Merwa Stefan <stefanbsns@gmail.com>
2023-02-08 22:46:10 +0100

    CodeAnalysis Project Initial TestEngine Implementation

---
+++ HB.Code.Analysis/Exceptions/CodeAnalyserTestException.cs
@@ -0,0 +1,16 @@
+﻿using HB.Common.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HB.Code.Analysis.Exceptions {
+    public class CodeAnalyserTestException : InternalException {
+        public CodeAnalyserTestException(string? message) : base(message) {
+        }
+
+        public CodeAnalyserTestException(string? message, Exception? innerException) : base(message, innerException) {
+        }
+    }
+}
---
+++ HB.Code.Analysis/Exceptions/CodeAnalyzerException.cs
@@ -0,0 +1,16 @@
+﻿using HB.Common.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HB.Code.Analysis.Exceptions {
+    public class CodeAnalyzerException : InternalException {
+        public CodeAnalyzerException(string? message) : base(message) {
+        }
+
+        public CodeAnalyzerException(string? message, Exception? innerException) : base(message, innerException) {
+        }
+    }
+}
