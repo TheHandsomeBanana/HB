db0ac0e42672ab7c8fda5fb9bd6c3209ee1928d3
Merwa Stefan <stefanbsns@gmail.com>
2023-05-08 19:50:25 +0200

    Attribute usage 1 max, NodeResolver Interface

---
+++ HB.NETF.Code.Analysis/INodeResolver.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.CodeAnalysis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HB.NETF.Code.Analysis {
+    public interface INodeResolver {
+        void ResolveNodeAsync(SyntaxNode node);
+    }
+}
--- HB.NETF.Discord.NET.Toolkit/SupportExtension/ServerIdListAttribute.cs
+++ HB.NETF.Discord.NET.Toolkit/SupportExtension/ServerIdListAttribute.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace HB.NETF.Discord.NET.Toolkit.SupportExtension {
-    [AttributeUsage(AttributeTargets.Assembly | AttributeTargets.Class)]
+    [AttributeUsage(AttributeTargets.Assembly | AttributeTargets.Class, AllowMultiple = false)]
     public class ServerIdListAttribute : Attribute {
         public ulong[] ServerIds { get; set; }
 
