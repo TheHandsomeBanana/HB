b4775bdd5ffc6908c632d74962821db8aa76e514
TheHandsomeBanana <stefanbsns@gmail.com>
2023-09-15 18:08:08 +0200

    Fix naming

--- HB.NETF.Services.Security/Identifier/Identifier.cs
+++ HB.NETF.Services.Security/Identifier/Identifier.cs
@@ -6,26 +6,26 @@ using System.Threading.Tasks;
 
 namespace HB.NETF.Services.Security.Identifier {
     public class Identifier : IIdentifier {
-        public Guid Identifier { get; }
+        public Guid Id { get; }
         public object Reference { get; }
         public Type ReferenceType { get; }
         public Identifier(object reference, Type referenceType) {
-            Identifier = Guid.NewGuid();
+            Id = Guid.NewGuid();
             Reference = reference;
             ReferenceType = referenceType;
         }
 
-        public bool Identify(Guid guid) => guid == Identifier;
+        public bool Identify(Guid guid) => guid == Id;
     }
 
     public class Identifier<TReference> : IIdentifier<TReference> {
-        public Guid Identifier { get; set; }
+        public Guid Id { get; set; }
         public TReference Reference { get; }
         public Identifier(TReference reference) {
-            Identifier = Guid.NewGuid();
+            Id = Guid.NewGuid();
             Reference = reference;
         }
 
-        public bool Identify(Guid guid) => guid == Identifier;
+        public bool Identify(Guid guid) => guid == Id;
     }
 }
