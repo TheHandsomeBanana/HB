f97c29ad19a5f38e45a86fd78a7fdcb3a3e00433
TheHandsomeBanana <stefanbsns@gmail.com>
2023-09-15 17:59:21 +0200

    IdentifierStreamHandler + Test

--- HB.NETF.Services.Security.Tests/DataHandlingTests.cs
+++ HB.NETF.Services.Security.Tests/DataHandlingTests.cs
@@ -1,6 +1,7 @@
 ﻿using HB.NETF.Services.Security.Cryptography;
 using HB.NETF.Services.Security.Cryptography.Keys;
 using HB.NETF.Services.Security.DataHandling;
+using HB.NETF.Services.Security.Identifier;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Microsoft.Win32.SafeHandles;
 using System;
@@ -23,6 +24,46 @@ namespace HB.Services.Security.Tests {
 
                 Assert.IsNotNull(key);
             }
+        }
+
+        [TestMethod]
+        public void TestIdentifierFileStream() {
+            IIdentifier<TestClass> idWrapper = new IdentifierObject<TestClass>(new TestClass("TestContent", 10));
+
+            using (FileStream fs = new FileStream("test", FileMode.OpenOrCreate, FileAccess.ReadWrite)) {
+                IdentifierStreamHandler<TestClass> streamHandler = new IdentifierStreamHandler<TestClass>(fs, typeof(IdentifierObject<TestClass>));
+                streamHandler.Write(idWrapper);
+
+                IIdentifier<TestClass> idWrapperRead = streamHandler.Read();
+
+                Assert.AreEqual(idWrapper.Reference.Count, idWrapperRead.Reference.Count);
+                Assert.AreEqual(idWrapper.Reference.Content, idWrapperRead.Reference.Content);
+            }
+        }
+
+        [TestMethod]
+        public void TestIdentifierFileStreamInvalid() {
+            ArgumentException e1 = Assert.ThrowsException<ArgumentException>(() => {
+                new IdentifierStreamHandler<TestClass>(typeof(string));
+            });
+
+            ArgumentException e2 = Assert.ThrowsException<ArgumentException>(() => {
+                new IdentifierStreamHandler<TestClass>(typeof(IIdentifier<string>));
+            });
+
+            string message = $"{typeof(string).FullName} does not inherit from {typeof(IIdentifier<TestClass>).FullName}";
+            Assert.AreEqual(e1.Message, message);
+            Assert.AreEqual(e2.Message, message);
+        }
+    }
+
+    public class TestClass {
+        public string Content { get; set; }
+        public int Count { get; set; }
+
+        public TestClass(string content, int count) {
+            Content = content;
+            Count = count;
         }
     }
 }
--- HB.NETF.Services.Security/DataHandling/IdentifierStreamHandler.cs
+++ HB.NETF.Services.Security/DataHandling/IdentifierStreamHandler.cs
@@ -1,30 +1,96 @@
-﻿using HB.NETF.Services.Security.Identifier;
+﻿using HB.NETF.Common.Serialization.Streams;
+using HB.NETF.Common;
+using HB.NETF.Services.Security.Cryptography.Keys;
+using HB.NETF.Services.Security.Identifier;
+using Microsoft.Win32;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace HB.NETF.Services.Security.DataHandling {
     public class IdentifierStreamHandler<TIdentifier> : SecurityStreamHandler, ISecurityStreamHandler<IIdentifier<TIdentifier>> {
+        private readonly Type identifierType;
+
+        public IdentifierStreamHandler(Type identifierType) {
+            if (!identifierType.GetInterfaces().Contains(typeof(IIdentifier<TIdentifier>)))
+                throw new ArgumentException($"{identifierType.FullName} does not inherit from {typeof(IIdentifier<TIdentifier>).FullName}");
+
+            this.identifierType = identifierType;
+       }
+
+        public IdentifierStreamHandler(FileStream stream, Type identifierType) : base(stream) {
+            if (!identifierType.GetInterfaces().Contains(typeof(IIdentifier<TIdentifier>)))
+                throw new ArgumentException($"{identifierType.FullName} does not inherit from {typeof(IIdentifier<TIdentifier>).FullName}");
+            
+            this.identifierType = identifierType;
+        }
+
+        public IdentifierStreamHandler(string filePath, Type identifierType) : base(filePath) {
+            if (!identifierType.GetInterfaces().Contains(typeof(IIdentifier<TIdentifier>)))
+                throw new ArgumentException($"{identifierType.FullName} does not inherit from {typeof(IIdentifier<TIdentifier>).FullName}");
+
+            this.identifierType = identifierType;
+        }
+
         public void Dispose() {
             this.Stream?.Dispose();
         }
 
         public IIdentifier<TIdentifier> Read() {
-            throw new NotImplementedException();
+            if (StreamMode == SecurityStreamMode.FileDialog) {
+                OpenFileDialog ofd = new OpenFileDialog();
+                ofd.Filter = "(*jk)|*jk";
+
+                if (!ofd.ShowDialog().Value)
+                    return null;
+
+                Stream = new FileStream(ofd.FileName, FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            }
+
+            string json = GlobalEnvironment.Encoding.GetString(Stream.Read());
+            Stream.Position = 0;
+
+            return JsonConvert.DeserializeObject(json, identifierType) as IIdentifier<TIdentifier>;
         }
 
-        public Task<IIdentifier<TIdentifier>> ReadAsync() {
-            throw new NotImplementedException();
+        public async Task<IIdentifier<TIdentifier>> ReadAsync() {
+            if (StreamMode == SecurityStreamMode.FileDialog)
+                throw new NotSupportedException($"{StreamMode} not supported in async execution.");
+
+            string json = GlobalEnvironment.Encoding.GetString(await Stream.ReadAsync());
+            Stream.Position = 0;
+
+            return JsonConvert.DeserializeObject(json, identifierType) as IIdentifier<TIdentifier>;
         }
 
         public void Write(IIdentifier<TIdentifier> item) {
-            throw new NotImplementedException();
+            if (StreamMode == SecurityStreamMode.FileDialog) {
+                SaveFileDialog sfd = new SaveFileDialog();
+                sfd.Filter = "(*jk)|*jk";
+                sfd.FileName = "hbkey";
+
+                if (!sfd.ShowDialog().Value)
+                    return;
+
+                Stream = new FileStream(sfd.FileName, FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            }
+
+            byte[] json = GlobalEnvironment.Encoding.GetBytes(JsonConvert.SerializeObject(item));
+            Stream.Write(json);
+            Stream.Position = 0;
         }
 
-        public Task WriteAsync(IIdentifier<TIdentifier> item) {
-            throw new NotImplementedException();
+        public async Task WriteAsync(IIdentifier<TIdentifier> item) {
+            if (StreamMode == SecurityStreamMode.FileDialog)
+                throw new NotSupportedException($"{StreamMode} not supported in async execution.");
+
+            byte[] json = GlobalEnvironment.Encoding.GetBytes(JsonConvert.SerializeObject(item));
+            await Stream.WriteAsync(json);
+            Stream.Position = 0;
         }
     }
 }
