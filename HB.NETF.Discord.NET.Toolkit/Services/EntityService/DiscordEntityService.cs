1efe9904917ba690c1f9858c494d880c7de1c669
Merwa Stefan <stefanbsns@gmail.com>
2023-11-11 23:05:09 +0100

    Command refactoring

--- HB.NETF.Discord.NET.Toolkit/Services/EntityService/DiscordEntityService.cs
+++ HB.NETF.Discord.NET.Toolkit/Services/EntityService/DiscordEntityService.cs
@@ -11,6 +11,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Runtime.Remoting.Channels;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
