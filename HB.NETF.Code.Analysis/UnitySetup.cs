ecaf3e46580517d04dd396cff6200778d781c126
Merwa Stefan <stefanbsns@gmail.com>
2023-12-21 15:51:23 +0100

    Bugfixes

--- HB.NETF.Code.Analysis/UnitySetup.cs
+++ HB.NETF.Code.Analysis/UnitySetup.cs
@@ -8,12 +8,6 @@ namespace HB.NETF.Code.Analysis {
     public class UnitySetup : IUnitySetup {
         public void Build(IUnityContainer container) {
             container.RegisterType<IAnalyserFactory, AnalyserFactory>();
-            IUnityContainer childContainer = container.CreateChildContainer();
-            childContainer.RegisterType<ITypeAnalyser, TypeAnalyser>()
-                .RegisterType<IVariableAnalyser, VariableAnalyser>()
-                .RegisterType<IIdentifierAnalyser, IdentifierAnalyser>();
-
-            container.RegisterInstance<IUnityContainer>("HB.NETF.Code.Analysis", childContainer);
         }
     }
 }
