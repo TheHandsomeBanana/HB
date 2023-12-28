2ce95ee26270fbcbd4e5bdcbca054f7b960a62e8
Merwa Stefan <stefanbsns@gmail.com>
2023-12-28 20:32:52 +0100

    Add semantic evaluator

---
+++ HB.Code.Interpreter/Evaluator/ISemanticEvaluator.cs
@@ -0,0 +1,13 @@
+﻿using HB.Code.Interpreter.Evaluator;
+using HB.Code.Interpreter.Syntax;
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HB.Code.Interpreter.Analyser;
+public interface ISemanticEvaluator<TSyntaxTree, TError> where TSyntaxTree : ISyntaxTree where TError : ISemanticError {
+    public ImmutableArray<TError> Evaluate(TSyntaxTree syntaxTree, string content);
+}
